7ca8f1e59f1ef2ea9d15ad6aa6c29ecdc8135cb0
breno-aredes <breno_mg10@hotmail.com>
2023-01-18 10:58:29 -0300

    feat: check if the user is registered
    1: post(/login)
    2: validation with JOI
    3: returns 422 if the password or login is wrong
    4: returns 200 if password and login are correct

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
 import joi from "joi";
+import e from "express";
 
 dotenv.config();
 
@@ -19,6 +20,51 @@ try {
   res.status(500).send("Erro no servidor", err.message);
 }
 const db = mongoClient.db();
+
+//get login so para teste, deletar depois.
+server.get("/login", async (req, res) => {
+  try {
+    const particiapants = await db.collection("login").find().toArray();
+    return res.send(particiapants);
+  } catch (err) {
+    res.status(500).send("Erro no servidor");
+  }
+});
+
+server.post("/login", async (req, res) => {
+  const { email, senha } = req.body;
+
+  const dataSchema = joi.object({
+    email: joi.string().email().required(),
+    senha: joi.string().min(5),
+  });
+
+  const validade = dataSchema.validate({ email, senha }, { abortEarly: false });
+
+  if (validade.err) {
+    const err = validade.err.details.map((detail) => detail.message);
+    return res.status(422).send(err);
+  }
+
+  try {
+    const verifyEmail = await db.collection("login").findOne({ email });
+
+    if (!verifyEmail) return res.send("e-mail não cadastrado").status(422);
+
+    if (verifyEmail.senha !== senha)
+      return res.send("senha errada").status(422);
+
+    return res.send("ok");
+  } catch {
+    return res.send("erro");
+  }
+});
+
+server.post("/sing-up", async (req, res) => {});
+
+server.get("/wallet", async (req, res) => {});
+
+server.post("/wallet", async (req, res) => {});
 
 const PORT = 5000;
 server.listen(PORT);
